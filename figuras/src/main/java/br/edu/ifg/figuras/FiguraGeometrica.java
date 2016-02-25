e0da0c54542acb5122ab4cfd29de9a7183583678
otaviocx <otaviocx@gmail.com>
2016-02-24 21:29:09 -0300

    Primeiro commit do projeto maven

---
+++ figuras/src/main/java/br/edu/ifg/figuras/FiguraGeometrica.java
@@ -0,0 +1,10 @@
+package br.edu.ifg.figuras;
+
+/**
+ *
+ * @author Otavio
+ */
+public abstract class FiguraGeometrica {
+    public abstract Long getArea();
+    public abstract Long getPerimetro();
+}
