a9ebf39fd05b5fb65382222d83848ee573fcd09a
otaviocx <otaviocx@gmail.com>
2016-03-03 21:49:35 -0300

    Alterações para documentação ...

--- figuras/src/main/java/br/edu/ifg/figuras/FiguraGeometrica.java
+++ figuras/src/main/java/br/edu/ifg/figuras/FiguraGeometrica.java
@@ -5,6 +5,11 @@ package br.edu.ifg.figuras;
  * @author Otavio
  */
 public abstract class FiguraGeometrica {
+    
+    /**
+     * Calcula a área da figura geométrica em questão
+     * @return A área da figura geométrica
+     */
     public abstract Long getArea();
     public abstract Long getPerimetro();
 }
